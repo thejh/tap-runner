5e33040e2d7afed500bdd15969006df2468f68b6
Jann Horn <jannhorn@googlemail.com>
2011-11-16 20:06:32 +0100

    don't nest so many calls

--- runner.js
+++ runner.js
@@ -46,11 +46,23 @@ Runner.prototype.runDir = function (dir, cb) {
 }
 
 Runner.prototype.runFiles = function (files, dir, cb) {
-
   var self = this
-  chain(files.map(function (f) { return function (cb) {
+  chain(files.map(makeFileHandler), cb)
+
+  return self
+  
+  function makeFileHandler(f) { return function (cb) {
     var relDir = dir || path.dirname(f)
       , fileName = relDir === "." ? f : f.substr(relDir.length + 1)
+      , cmd
+      , args = []
+      , env = {}
+      , err = ""
+      , out = ""
+      , tc = new TapConsumer
+    
+    for (var i in process.env) env[i] = process.env[i]
+    env.TAP = 1
 
     self.write(fileName)
     fs.lstat(f, function (er, st) {
@@ -60,8 +72,7 @@ Runner.prototype.runFiles = function (files, dir, cb) {
         return cb()
       }
 
-      var cmd = f
-        , args = []
+      cmd = f
 
       if (path.extname(f) === ".js") {
         cmd = "node"
@@ -73,57 +84,52 @@ Runner.prototype.runFiles = function (files, dir, cb) {
       if (st.isDirectory()) {
         return self.runDir(f, cb)
       }
-
-      var env = {}
-      for (var i in process.env) env[i] = process.env[i]
-      env.TAP = 1
       
-      fs.readFile(f, 'utf8', function(er, fileSource) {
-        if (er) {
-          self.write(assert.fail("could not read "+f, {error: er}))
-          return cb()
-        }
-        if (fileSource.slice(0, 2) === '#!') {
-          args = fileSource.split('\n')[0].slice(2).trim().split(' ')
-          cmd = args.shift()
-          args.push(fileName)
-        }
-        var cp = child_process.spawn(cmd, args, { env: env, cwd: relDir })
-          , out = ""
-          , err = ""
-          , tc = new TapConsumer
-          , childTests = [f]
-
-        tc.on("data", function (c) {
-          self.emit("result", c)
-          self.write(c)
-        })
-
-        cp.stdout.pipe(tc)
-        cp.stdout.on("data", function (c) { out += c })
-        cp.stderr.on("data", function (c) { err += c })
-
-        cp.on("exit", function (code) {
-          //childTests.forEach(function (c) { self.write(c) })
-          var res = { name: fileName
-                    , ok: !code }
-          if (err) {
-            res.stderr = err
-            if (tc.results.ok && tc.results.tests === 0) {
-              // perhaps a compilation error or something else failed...
-              console.error(err)
-            }
-          }
-          res.command = [cmd].concat(args).map(JSON.stringify).join(" ")
-          self.emit("result", res)
-          self.emit("file", f, res, tc.results)
-          self.write(res)
-          self.write("\n")
-          cb()
-        })
-      })
+      fs.readFile(f, 'utf8', runFile)
     })
-  }}), cb)
+    
+    function runFile(er, fileSource) {
+      if (er) {
+        self.write(assert.fail("could not read "+f, {error: er}))
+        return cb()
+      }
+      if (fileSource.slice(0, 2) === '#!') {
+        args = fileSource.split('\n')[0].slice(2).trim().split(' ')
+        cmd = args.shift()
+        args.push(fileName)
+      }
+      var cp = child_process.spawn(cmd, args, { env: env, cwd: relDir })
+        , childTests = [f]
 
-  return self
+      tc.on("data", function (c) {
+        self.emit("result", c)
+        self.write(c)
+      })
+
+      cp.stdout.pipe(tc)
+      cp.stdout.on("data", function (c) { out += c })
+      cp.stderr.on("data", function (c) { err += c })
+
+      cp.on("exit", fileRan)
+    }
+    
+    function fileRan(code) {
+      //childTests.forEach(function (c) { self.write(c) })
+      var res = { name: fileName
+                , ok: !code }
+      if (err) {
+        res.stderr = err
+        if (tc.results.ok && tc.results.tests === 0) {
+          // perhaps a compilation error or something else failed...
+          console.error(err)
+        }
+      }
+      res.command = [cmd].concat(args).map(JSON.stringify).join(" ")
+      self.emit("result", res)
+      self.emit("file", f, res, tc.results)
+      self.write(res)
+      self.write("\n")
+      cb()
+    }
+  }}
 }
